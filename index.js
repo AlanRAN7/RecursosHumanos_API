fef9a4c0ce61c65eb4a25b06641aed753fc5894e
Alan Álvarez <aran.712@gmail.com>
2022-05-10 22:15:02 -0500

    added: Morgan, POST

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
+
+// middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 //Routes
 const employee = require('./routes/employee');
+
 
 app.get('/', (req, res) => {
   res.status(200);
--- routes/employee.js
+++ routes/employee.js
@@ -2,10 +2,35 @@ const express = require('express');
 const employee = express.Router();
 const db = require('../config/database');
 
-employee.get('/', async(req, res, next) =>{
+employee.get('/', async (req, res, next) =>{
     const emp = await db.query("SELECT * FROM employee");
     return res.status(200).json({code:200, message:emp});
 });
+
+employee.post('/', (req,res,next) =>{
+    return res.status(200).send(req.body)
+})
+
+// employee.post("/", async (req,res,next) => {
+//     const { name, last_name, phone_number, email, address } = req.body;
+
+//     if (name && last_name && phone_number && email && address) {
+//       let query =
+//         "INSERT INTO employee (name, last_name, phone_number, email, address)";
+//       query += `VALUES('${name}', ${last_name}, ${phone_number}, ${email}, ${address})`;
+//       const rows = await db.query(query);
+//       console.log(rows);
+  
+//       if (rows.affectedRows == 1) {
+//         return res.status(201).json({
+//           code: 201,
+//           message: "Pokemon insertado correctamente",
+//         });
+//       }
+//       return res.status(500).json({ code: 500, message: "Ocurrió un error" });
+//     }
+//     return res.status(500).json({ code: 500, message: "Campos incompletos" });
+// });
 
 employee.get('/:id([0-9]{1,3})', async(req, res, next) =>{
     
@@ -21,6 +46,9 @@ employee.get('/:name([A-Za-z]+)', async(req, res, next) =>{
     const name = req.params.name;
     const emp = await db.query("SELECT * FROM employee WHERE name='" + name + "';")
 
+    // const empInfo = empInfo.filter((eInfo) => {
+    //     return(eInfo.name.toUpperCase()== name.toUpperCase()) && eInfo;
+    // })
     if (emp.length > 0) {
        return res.status(200).json({code:200, message: emp})
     } 
